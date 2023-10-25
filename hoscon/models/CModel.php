c9bd6a2699b27daa00f00be330cdcfcf4595fd33
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2023-10-25 14:46:41 +0300

    Add files via upload

---
+++ hoscon/models/CModel.php
@@ -0,0 +1,20 @@
+<?php
+class Cmodel {
+    public function GetData(){
+        $arrayResult[]= array(
+            'TITLE'=>'Blor hosting site',
+            'DATE'=>'20.06.2020',
+            'AUTHOR'=>'VolDeMort',
+            'IMAGE'=>'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
+            'TEXT'=>'Blor hosting site',
+        );
+        $arrayResult[]= array(
+            'TITLE'=>'Blor hosting site',
+            'DATE'=>'20.06.2021',
+            'AUTHOR'=>'igor',
+            'IMAGE'=>'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
+            'TEXT'=>'Blor hosting site',
+        );
+        return $arrayResult;
+    }
+}
